eafac2fc87c7c9f51629dc6ea073129c8c3ea7a6
879307253 <879307253@qq.com>
2019-04-08 19:13:03 +0800

    自动输入汇率
    Signed-off-by: 879307253 <879307253@qq.com>

--- src/main/java/com/example/a87939/myapplication/RateActivity.java
+++ src/main/java/com/example/a87939/myapplication/RateActivity.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class RateActivity extends AppCompatActivity {
-EditText rmb;
+EditText rmb,dol,eur,han;
 TextView show;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,28 +16,54 @@ TextView show;
         setContentView(R.layout.activity_rate);
 
         rmb=findViewById(R.id.rmb);
+        dol=findViewById(R.id.dol);
+        eur=findViewById(R.id.eur);
+        han=findViewById(R.id.han);
         show=findViewById(R.id.showOut);
     }
     public void onClick(View btn){
         //获取用户输入内容
         String str = rmb.getText().toString();
         float r =0;
+        float d=0,e=0,h=0;
+        String str1 = dol.getText().toString();
+        String str2 = eur.getText().toString();
+        String str3 = han.getText().toString();
+
         if(str.length()>0){
          r=Float.parseFloat(str);
         }
         else{
             Toast.makeText(this,"请输入金额",Toast.LENGTH_LONG).show();
         }
-        float val=0;
-        if(btn.getId()==R.id.btn_dollar){
-            val=r*(1/6.7f);
-        }
-        else if(btn.getId()==R.id.btn_euro){
-             val=r*(1/11.0f);
+        if(str1.length()>0){
+            d=Float.parseFloat(str1);
         }
         else{
-             val=r*500;
+            Toast.makeText(this,"请输入汇率",Toast.LENGTH_LONG).show();
+        }
+        if(str2.length()>0){
+            e=Float.parseFloat(str2);
+        }
+        else{
+            Toast.makeText(this,"请输入汇率",Toast.LENGTH_LONG).show();
+        }
+        if(str3.length()>0){
+            h=Float.parseFloat(str3);
+        }
+        else{
+            Toast.makeText(this,"请输入汇率",Toast.LENGTH_LONG).show();
+        }
+        float val=0;
 
+        if(btn.getId()==R.id.btn_dollar){
+            val=r*d;
+        }
+        else if(btn.getId()==R.id.btn_euro){
+            val=r*e;
+        }
+        else if(btn.getId()==R.id.btn_won){
+            val=r*h;
         }
         String s=String.format("%.2f", val);
         show.setText(s);
