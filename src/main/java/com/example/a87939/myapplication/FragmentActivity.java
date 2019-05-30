af58cd60f4b1c6aaf2ff03912cb6981f34fb57e5
879307253 <879307253@qq.com>
2019-05-30 16:21:41 +0800

    fragment1

---
+++ src/main/java/com/example/a87939/myapplication/FragmentActivity.java
@@ -0,0 +1,69 @@
+package com.example.a87939.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+public class FragmentActivity extends android.support.v4.app.FragmentActivity {
+    private Fragment mFragments[];
+    private RadioGroup radioGroup;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
+    private RadioButton rbtHome,rbtFunc,rbtSetting;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+        mFragments =new Fragment[3];
+        fragmentManager=getSupportFragmentManager();
+        mFragments[0]=fragmentManager.findFragmentById(R.id.fragment_main);
+        mFragments[1]=fragmentManager.findFragmentById(R.id.fragment_func);
+        mFragments[2]=fragmentManager.findFragmentById(R.id.fragment_setting);
+        fragmentTransaction=
+                fragmentManager.beginTransaction().hide(mFragments[0]).hide(mFragments[1]).hide(mFragments[2]);
+        fragmentTransaction.show(mFragments[0]).commit();
+        rbtHome = (RadioButton)findViewById(R.id.radioHome);
+        rbtFunc = (RadioButton)findViewById(R.id.radioFunc);
+        rbtSetting = (RadioButton)findViewById(R.id.radioSetting);
+        rbtHome.setBackgroundResource(R.drawable.shape3);
+
+
+        //切换界面
+        radioGroup=(RadioGroup)findViewById(R.id.bottomGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                Log.i("radioGroup", "checkId=" + checkedId);
+                fragmentTransaction =
+                        fragmentManager.beginTransaction().hide(mFragments[0]).hide(mFragments[1]).hide(mFragments[2]);
+                rbtHome.setBackgroundResource(R.drawable.shape2);
+                rbtFunc.setBackgroundResource(R.drawable.shape2);
+                rbtSetting.setBackgroundResource(R.drawable.shape2);
+
+
+                switch(checkedId){
+                    case R.id.radioHome:
+                        fragmentTransaction.show(mFragments[0]).commit();
+                        rbtHome.setBackgroundResource(R.drawable.shape3);
+                        break;
+                    case R.id.radioFunc:
+                        fragmentTransaction.show(mFragments[1]).commit();
+                        rbtHome.setBackgroundResource(R.drawable.shape3);
+                        break;
+                    case R.id.radioSetting:
+                        fragmentTransaction.show(mFragments[2]).commit();
+                        rbtHome.setBackgroundResource(R.drawable.shape3);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+    }
+}
---
+++ src/main/java/com/example/a87939/myapplication/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.example.a87939.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class HomeFragment extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        return  inflater.inflate(R.layout.frame_home,container);
+
+    }
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        TextView tv=(TextView)getView().findViewById(R.id.homeTextView1);
+        tv.setText("这是主页面");
+    }
+}
